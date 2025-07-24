500a5c0f21029e32ca0ea3ae3b34f06cd6ea5ef9
Samarth DevTools <samarthm@devtools.in>
2025-07-24 07:07:55 +0000

    modified

--- integration-testing-ec2.sh
+++ integration-testing-ec2.sh
@@ -5,16 +5,16 @@ aws --version
 
 Data=$(aws ec2 describe-instances)
 echo "Data "$Data
-URL=$(aws ec2 describe-instances jq -r. Reservations []. Instances [] | select(.Tags [].Value == "dev_deploy") |
+URL=$(aws ec2 describe-instances | jq -r ' .Reservations[].Instances[] | select(.Tags[].Value == "dev_deploy") | .PublicDnsName')
 echo "URL Data - "$URL
 
-if [[ "$URL" != '"]]; then
-    http_code=$(curl -s -o/dev/null -w "%{http_code)" http://$URL:3000/live
-        echo "http_code "$http_code
+if [[ "$URL" != '' ]]; then
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" http://$URL:3000/live)
+        echo "http_code - "$http_code
     planet_data=$(curl -s -XPOST http://$URL:3000/planet -H "Content-Type: application/json" -d "{"id": "3"}')
-        echo "planet_data "$planet_data
-    planet_name=$(echo $planet_data jq.name -r)
-        echo "planet_name "$planet_name
+        echo "planet_data - "$planet_data
+    planet_name=$(echo $planet_data | jq .name -r)
+        echo "planet_name - "$planet_name
     
     if [[ "$http_code" -eq 200 && "$planet_name" -eq "Earth" ]];
         then
